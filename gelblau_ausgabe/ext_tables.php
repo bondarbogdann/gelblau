cf5b71f4e6f63727233b141c64a40796ca908fca
Bogdan Bondar <bondarbogdann@gmail.com>
2017-08-05 00:13:11 +0200

    added switchable actions to ausgabe plugin

--- gelblau_ausgabe/ext_tables.php
+++ gelblau_ausgabe/ext_tables.php
@@ -11,6 +11,10 @@ call_user_func(
             'Ausgabe'
         );
 
+        $pluginSignature = str_replace('_', '', 'gelblau_ausgabe') . '_ausgabe';
+        $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:gelblau_ausgabe/Configuration/FlexForms/flexform_ausgabe.xml');
+
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('gelblau_ausgabe', 'Configuration/TypoScript', 'Gelblau Ausgabe');
 
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_gelblauausgabe_domain_model_ausgabe', 'EXT:gelblau_ausgabe/Resources/Private/Language/locallang_csh_tx_gelblauausgabe_domain_model_ausgabe.xlf');
